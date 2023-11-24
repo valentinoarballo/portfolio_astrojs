95e07227e8a076bfdd1b508ee612817d4325e8b9
valentino <v.arballo@itecriocuarto.org.ar>
2023-11-24 19:15:13 -0300

    gap fixed in hero, scroll snap gone

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		fontFamily: {
+		  'thunder': ['Thunder', 'sans-serif'],
+		}
+	  },
 	},
 	plugins: [],
-}
+  }
+  
